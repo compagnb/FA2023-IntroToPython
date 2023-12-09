10e9409306a98560f315c9fcb987c6528a3ba066
Barbara Compagnoni <5366712+compagnb@users.noreply.github.com>
2023-12-09 11:14:02 -0500

    12/9/2023

---
+++ classFiles/david/week7.py
@@ -0,0 +1,23 @@
+#Week 7
+
+import turtle
+
+vel=3
+
+grinch = turtle.Turtle()
+grinch.color("green")
+grinch.penup()
+grinch.speed(0)
+
+while True:
+    grinch.forward(vel)
+    if grinch.xcor()>300 or grinch.xcor()<-300:
+        grinch.left(30)
+    if grinch.ycor()>300 or grinch.ycor()<-300:
+        grinch.left(30)
+
+
+
+
+
+
---
+++ classFiles/david/week7b.py
@@ -0,0 +1,18 @@
+# Week 7b
+import turtle
+import random
+
+grinches=[]
+vel=random.randint(1,5)
+numOfGrinches=10
+
+for i in range(0,numOfGrinches):
+    grinches.append(turtle.Turtle())
+    grinches[i].color("green")
+    grinches[i].penup()
+    grinches[i].speed()
+    grinches[i].goto(random.randint(-300,300),random.randint(-300,300))
+    grinches[i].setheading(random.randint(0,360))
+
+#while True:
+                           
